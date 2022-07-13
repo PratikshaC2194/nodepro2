5f9bde9804985c00aae963e7dc6a42e618a96eb4
PratikshaC2194 <97968229+PratikshaC2194@users.noreply.github.com>
2022-07-13 06:17:35 -0700

    Add files via upload

---
+++ datas.js
@@ -0,0 +1,27 @@
+const datas=(req,res)=>{
+    res.send([{
+      "id":"1",
+      "Title": " Vicky Kaushal and Katrina Kaif to Celebrate First Lohri After Marriage, Actor Enjoys Jalebi in Indore - News18 ",
+      "CategoryName": "Bollywood",
+      "PublishedDate": "  January  / 15    2022",
+      "ImageAsset": "https://images.news18.com/ibnlive/uploads/2021/12/katrina-and-vicky-pic-163999921816x9.jpg  ",
+      "BlogContent": "The Uri actor, who is in Indore for his work commitments, recently took to his Instagram Story section to share a glimpse of him enjoying sweets before the festivities.",
+    },
+    {
+      "Title": "Christmas 2019 recipes: Make plum pudding and mince pie for your child ",
+      "CategoryName": "FOOD",
+      "PublishedDate": "  January  / 15   2019",
+      "ImageAsset": "https://images.indianexpress.com/2019/12/Traditional-Plum-Pudding-at-Novotel-ibis-Bengaluru-Outer-Ring-Road.jpg?resize=450,250 ",
+      "BlogContent": " Christmas 2019: No festival is complete without gorging on delicious treats. Make these dishes at home and enjoy with your family",
+    },
+      {
+        "Title": " ",
+        "CategoryName": "FITNESS",
+        "PublishedDate": " Januaury 13 2021",
+        "ImageAsset": " http://images.indianexpress.com/2021/10/cardio_1200_getty.jpg?resize=450,250",
+        "BlogContent": "  You may find it hard to wake up in the morning or struggle to sleep early at night, but workout in the evening is not for you,ayurvedic practitioner Dr Varalakshmi Yanamandra said",
+      }
+    ])
+ 
+ }
+ module.exports.datas=datas;
